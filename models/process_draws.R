4c1cc470c7b59fa3a779fff91ea2121406c62a7b
Ewan Carr <ewan.carr@kcl.ac.uk>
2022-02-22 13:52:29 +0000

    Process posteriors from updated models; make tables/figures

--- models/process_draws.R
+++ models/process_draws.R
@@ -1,4 +1,4 @@
-# Title:        Process posterior draws from a/b/c-path models
+# Title:        Process posterior draws from H1/H2/H3 models
 # Author:       Ewan Carr
 # Started:      2022-01-13
 
@@ -9,12 +9,15 @@ library(here)
 library(tidybayes)
 library(broom)
 library(broom.mixed)
+library(rstanarm)
 library(brms)
 library(emmeans)
 library(tidybayes)
 library(patchwork)
+library(RColorBrewer)
+library(colorspace)
 load(here("data", "clean", "sel.Rdata"), verbose = TRUE)
-load(here("outputs", "posteriors.Rdata"), verbose = TRUE)
+ff <- "Arial"
 
 extract_draws <- function(list_of_brmsfit) {
   post <- map_dfr(list_of_brmsfit, function(x) {
@@ -30,18 +33,6 @@ extract_draws <- function(list_of_brmsfit) {
   return(post)
 }
 
-map(list(a = a_path), extract_draws)
-map(list(b = b_path), extract_draws)
-map(list(c = c_path), extract_draws)
-
-p <- map(list(a = a_path, b = b_path, c = c_path), extract_draws) %>%
-       map(~ mutate(.x, model = str_replace_all(model, "1sd|l3|of", ""))) %>%
-       map_dfr(~ separate(.x, model, c("left", "right")), .id = "path") %>%
-       group_by(path, left, right) %>%
-       summarise(median_qi(.value)) %>%
-       mutate(cell = str_glue("{sprintf('%.2f', y)} [{sprintf('%.2f', ymin)}, {sprintf('%.2f', ymax)}]")) %>%
-       select(path, left, right, cell) 
-
 reshape_table <- function(tab, p = "a") { 
   tab <- filter(tab, path == p) %>% 
     spread(right, cell) %>%
@@ -55,14 +46,17 @@ reshape_table <- function(tab, p = "a") {
 ####                                                                      #####
 ###############################################################################
 
+load(here("outputs", "fit_h2.Rdata"), verbose = TRUE)
+load(here("outputs", "fit_h3.Rdata"), verbose = TRUE)
+
 # Gather together models where 'wear_time' is the outcome
-wear_time <- list(fit = list(b_path[[5]], b_path[[6]], b_path[[7]], b_path[[8]],
-                             c_path[[1]], c_path[[2]], c_path[[3]]),
-                  x = c(# b-paths
+wear_time <- list(fit = list(fit_h2[[5]], fit_h2[[6]], fit_h2[[7]], fit_h2[[8]],
+                             fit_h3[[1]], fit_h3[[2]], fit_h3[[3]]),
+                  x = c(# H2
                         "psurev_1sd", "tamrev_1sd", "ease_1sd", "useful_1sd",
-                        # c-paths
+                        # H3
                         "ids_1sd", "gad_1sd", "wsas_1sd"),
-                  label = c(rep("b_path", 4), rep("c_path", 3)))
+                  label = c(rep("h2", 4), rep("h3", 3)))
 
 # Extract model predictions
 extract_effect <- function(fit, param, diff = c(-0.5, 0.5)) {
@@ -83,48 +77,54 @@ post_wt <- pmap_dfr(wear_time, function(fit, x, label) {
 
 integer_breaks <- function(x) seq(ceiling(x[1]), floor(x[2]), by = 1)
 
-# Figure
-plot_wt <- post_wt %>%
-  ggplot(aes(x = exposure,
-             y = .value,
-             color = ordered(x))) +
-  stat_lineribbon(alpha = 0.5, size = 0.75) +
-  facet_nested_wrap(~ path + x,
-                    scale = "free_x",
-                    ncol = 4) +
-  scale_fill_brewer(palette = "Greys") +
-  scale_color_brewer(type = "qual") +
-  theme_minimal(base_family = "Times New Roman") + 
+labels <- tribble(~x,           ~label,   ~sub,
+                  "psurev_1sd", "PSSUQ",  "Total score",
+                  "tamrev_1sd", "TAM-FF", "Total score",
+                  "useful_1sd", "TAM-FF", "Perceived usefulness",
+                  "ease_1sd",   "TAM-FF", "Perceived ease of use",
+                  "gad_1sd",    "GAD-7",  "Total score",
+                  "ids_1sd",    "IDS-SR", "Total score",
+                  "wsas_1sd",   "WSAS",   "Total score")
+labels$label <- factor(labels$label, levels = c("PSSUQ", "TAM-FF", "IDS-SR", "GAD-7", "WSAS"))
+labels$sub <- factor(labels$sub, levels = c("Total score", "Perceived ease of use", "Perceived usefulness"))
+plot_data <- left_join(post_wt, labels)
+
+pc <- c(tail(brewer.pal(6, "Blues"), 2), 
+        tail(brewer.pal(6, "Purples"), 2), 
+        brewer.pal(3, "Set2"))
+col <- c("PSSUQ"  = pc[2],
+         "TAM-FF" = pc[1],
+         "TAM-FF" = pc[3],
+         "TAM-FF" = pc[4],
+         "GAD-7"  = pc[5],
+         "IDS-SR" = pc[6],
+         "WSAS"   = pc[7])
+
+plot_wt <- plot_data %>%
+  ggplot() +
+  aes(x = exposure,
+      fill = label,
+      color = label,
+      y = .value) +
+  stat_lineribbon(.width = c(0.95),
+                  point_interval = "median_qi") +
+  facet_nested_wrap(~ label + sub, ncol = 4) +
+  theme_minimal(base_family = ff) + 
   theme(legend.position = "none") +
   scale_x_continuous(breaks = integer_breaks) +
   lims(y = c(0, 1)) +
-  labs(y = "Proportion FitBit wear time")
+  labs(y = "Proportion of time wearing FitBit, next 3 months",
+       x = "Standard deviations difference in exposure") +
+  theme(strip.background = element_rect(fill = "gray85", line = 0)) +
+  scale_color_manual(values = col) +
+  scale_fill_manual(values = map(col, lighten, 0.8))
 
 ggsave(plot_wt,
-       filename = here("writing", "figures", "wear_time.png"),
+       filename = here("writing", "figures", "figs3_weartime.png"),
        dev = "png",
-       width = 6,
-       height = 5,
+       width = 8,
+       height = 6,
        dpi = 300)
-
-
-# Table
-post_wt %>%
-  group_by(path, x) %>%
-  mutate(mid = median(exposure),
-         lo = mid - 0.5,
-         hi = mid + 0.5) %>%
-  filter(exposure == lo | exposure == hi) %>%
-  group_by(path, x) %>%
-  mutate(hilo = case_when(exposure == min(exposure) ~ FALSE,
-                          exposure == max(exposure) ~ TRUE)) %>%
-  ungroup() %>%
-  select(.draw, hilo, .value, path, x) %>%
-  spread(hilo, .value) %>%
-  mutate(diff = `TRUE` - `FALSE`) %>%
-  group_by(path, x) %>%
-  summarise(diff = median_qi(diff))
-
 
 ###############################################################################
 ####                                                                      #####
@@ -132,16 +132,16 @@ post_wt %>%
 ####                                                                      #####
 ###############################################################################
 
-total_phq8 <- list(fit = list(b_path[[1]], b_path[[2]], 
-                              b_path[[3]], b_path[[4]],
-                              c_path[[4]], c_path[[5]],
-                              c_path[[6]]),
-                  x = c(# b-paths
-                        "psurev_1sd", "tamrev_1sd", "ease_1sd", "useful_1sd",
-                        # c-paths
-                        "ids_1sd", "gad_1sd", "wsas_1sd"),
-                  label = c(rep("b_path", 4), rep("c_path", 3)),
-                  xr = as.logical(c(1, 1, 0, 0, 1, 1, 1)))
+n_phq8 <- list(fit = list(fit_h2[[1]], fit_h2[[2]], 
+                          fit_h2[[3]], fit_h2[[4]],
+                          fit_h3[[4]], fit_h3[[5]],
+                          fit_h3[[6]]),
+               x = c(# H2
+                     "psurev_1sd", "tamrev_1sd", "ease_1sd", "useful_1sd",
+                     # H3
+                     "ids_1sd", "gad_1sd", "wsas_1sd"),
+               label = c(rep("h2", 4), rep("h3", 3)),
+               xr = as.logical(c(1, 1, 0, 0, 1, 1, 1)))
 
 get_draws <- function(model,
                       var,
@@ -159,6 +159,7 @@ get_draws <- function(model,
                            wsas = gm(data$wsas),
                            inwork = gm(data$inwork),
                            comorf = "0",
+                           n_lte_f = "0",
                            gad_1sd = gm(data$gad_1sd),
                            wsas_1sd = gm(data$wsas_1sd),
                            ids_1sd = gm(data$ids_1sd),
@@ -180,58 +181,61 @@ get_draws <- function(model,
 }
 
 
-post_phq8 <- pmap_dfr(total_phq8, 
+post_phq8 <- pmap_dfr(n_phq8, 
      function(fit, x, label, xr) {
        d <- get_draws(model = fit, 
                       var = x, 
                       data = sel,
                       use_1sd = xr)
        d$model <- label
-       d$label <- x
+       d$x <- x
        return(d) 
      }) %>%
   ungroup()
 
+plot_data <- left_join(post_phq8, labels)
+
 plot_phq8 <- post_phq8 %>%
-  ggplot(aes(x = exposure,
-             y = mean_count,
-             color = label)) +
-  stat_lineribbon(size = 1,
-                  point_interval = "median_qi",
-                  .width = c(0.5, 0.8, 0.95)) +
-  theme_minimal(base_family = "Times New Roman") +
-  scale_fill_brewer(palette = "Greys") +
-  facet_nested_wrap(model ~ label,
-             scales = "free_x", 
-             ncol = 4) +
+  left_join(labels) %>%
+  ggplot() +
+  aes(x = exposure,
+      y = mean_count,
+      fill = label,
+      color = label) +
+  stat_lineribbon(.width = 0.95,
+                  point_interval = "median_qi") +
+  facet_nested_wrap(~ label + sub, ncol = 4) +
+  theme_minimal(base_family = ff) + 
   theme(legend.position = "none") +
-  scale_y_continuous(limits = c(0, 7), 
-                     breaks = 0:7,
-                     labels = ~ if_else(.x < 7,
-                                        as.character(.x),
-                                        "7+")) +
   scale_x_continuous(breaks = integer_breaks) +
-  labs(y = "Predicted number of PHQ-8 questionnaires completed")
+  scale_y_continuous(limits = c(0, 6), 
+                     breaks = 0:6) +
+  labs(y = "Number of PHQ-8 questionnaires completed",
+       x = "Standard deviations difference in exposure") +
+  theme(strip.background = element_rect(fill = "gray85", line = 0)) +
+  scale_color_manual(values = col) +
+  scale_fill_manual(values = map(col, lighten, 0.8))
 
 ggsave(plot_phq8,
-       filename = here("writing", "figures", "phq8.png"),
+       filename = here("writing", "figures", "figs2_phq8.png"),
        dev = "png",
-       width = 6,
-       height = 5,
+       width = 8,
+       height = 6,
        dpi = 300)
 
-
 ###############################################################################
 ####                                                                      #####
-####                         Models for mediators                         #####
+####                 Models for usability and acceptability               #####
 ####                                                                      #####
 ###############################################################################
 
-med <- list(fit = a_path,
-            x = rep(c("wsas_1sd", "ids_1sd", "gad_1sd"), each = 4),
-            y = rep(c("psu", "tam", "useful", "ease"), 3))
+load(here("outputs", "fit_h1.Rdata"), verbose = TRUE)
 
-post_med <- pmap_dfr(med, function(fit, x, y) {
+usab <- list(fit = fit_h1,
+           x = rep(c("wsas_1sd", "ids_1sd", "gad_1sd"), each = 4),
+           y = rep(c("psu", "tam", "useful", "ease"), 3))
+
+post_usab <- pmap_dfr(usab, function(fit, x, y) {
                        extract_effect(fit, x, seq(-1, 1, 0.5)) %>%
                          mutate(y = y,
                                 x = x)
@@ -242,43 +246,50 @@ post_med <- pmap_dfr(med, function(fit, x, y) {
             y == "tam" ~ "TAM-FF total (0-112; reversed)",
             y == "psu" ~ "PSSUQ total, (0-114; reversed)")) 
 
+plot_data <- left_join(post_usab, labels)
 
-make_mediator_plot <- function(data) {
+make_mediator_plot <- function(data, colors) {
   data %>%
     ggplot(aes(x = exposure,
                y = .value,
-               color = x)) +
-    stat_lineribbon(size = 1,
-                    point_interval = "median_qi",
-                    .width = c(0.5, 0.8, 0.95)) +
-    theme_minimal(base_family = "Times New Roman") +
-    scale_fill_brewer(palette = "Greys") +
-    facet_nested_wrap(y_label ~ x,
+               color = label,
+               fill = label)) +
+    stat_lineribbon(point_interval = "median_qi",
+                    .width = 0.95) +
+    theme_minimal(base_family = ff) +
+    facet_nested_wrap(y_label ~ label,
                       scales = "free_x", 
                       ncol = 6) +
-    theme(legend.position = "none") +
-    scale_color_brewer(type = "qual")
+    theme(legend.position = "none",
+          strip.background = element_rect(fill = "gray85", line = 0)) +
+    scale_color_manual(values = colors) +
+    scale_fill_manual(values = map(colors, lighten, 0.8))
 }
 
-top_plot <- post_med %>%
+bottom_plot <- plot_data %>%
   filter(y %in% c("ease", "useful")) %>%
-  make_mediator_plot() +
+  make_mediator_plot(col) +
   scale_x_continuous(limits = c(-1, 1), breaks = integer_breaks) +
-  scale_y_continuous(limits = c(0, 7), breaks = integer_breaks) 
+  scale_y_continuous(limits = c(0, 7), breaks = integer_breaks) +
+  theme(axis.title.x = element_text(margin = margin(0.4, 0, 0, 0, "cm"))) +
+  labs(y = "Mean score",
+       x = "Standard deviations difference in exposure")
 
-bottom_plot <- post_med %>%
+top_plot <- plot_data %>%
   filter(y %in% c("tam", "psu")) %>%
-  make_mediator_plot() +
+  make_mediator_plot(col) +
   scale_x_continuous(limits = c(-1, 1), breaks = integer_breaks) +
-  scale_y_continuous(limits = c(0, 114)) 
+  scale_y_continuous(limits = c(0, 114)) +
+  labs(y = "Mean score") +
+  theme(axis.title.x = element_blank())
 
-plot_med <- top_plot / bottom_plot
+plot_usab <- top_plot / bottom_plot
 
-ggsave(plot_med,
-       filename = here("writing", "figures", "mediators.png"),
+ggsave(plot_usab,
+       filename = here("writing", "figures", "figs1_usab.png"),
        dev = "png",
-       width = 6,
-       height = 5,
+       width = 8,
+       height = 6,
        dpi = 300)
 
 ###############################################################################
@@ -293,8 +304,8 @@ make_cell <- function(est, lo, hi) {
                 str_glue("{sprintf('%.2f', hi)}]")))
 }
 
-# Assumble results for a-paths
-tab_med <- post_med %>%
+# Assumble results for "usability and acceptability" outcomes
+tab_usab <- post_usab %>%
   filter(exposure %in% c(-0.5, 0.5)) %>%
   select(.draw, .value, y, y_label, exposure, x)  %>%
   spread(exposure, .value) %>%
@@ -303,24 +314,24 @@ tab_med <- post_med %>%
   median_qi(contrast) %>%
   mutate(cell = make_cell(contrast, .lower, .upper)) %>%
   select(y, x, cell) %>%
-  mutate(id = "a_path")
+  mutate(id = "usab")
 
 a <- median(sel$ease_1sd, na.rm = TRUE)
 b <- median(sel$useful_1sd, na.rm = TRUE)
 
 # Assemble results for "Total PHQ-8 completions"
 tab_phq8 <- post_phq8 %>%
-  mutate(id = case_when((str_ends(label, "_1sd") & exposure == -0.5) ~ FALSE,
-                        (str_ends(label, "_1sd") & exposure == 0.5) ~ TRUE)) %>%
+  mutate(id = case_when((str_ends(x, "_1sd") & exposure == -0.5) ~ FALSE,
+                        (str_ends(x, "_1sd") & exposure == 0.5) ~ TRUE)) %>%
   drop_na(id) %>%
-  select(.draw, id, mean_count, model, label) %>%
+  select(.draw, id, mean_count, model, x) %>%
   spread(id, mean_count) %>%
   mutate(contrast = `TRUE` - `FALSE`) %>%
-  group_by(model, label) %>%
+  group_by(model, x) %>%
   median_qi(contrast) %>% 
   mutate(id = "phq8",
          cell = make_cell(contrast, .lower, .upper)) %>%
-  select(id, path = model, x = label, cell) 
+  select(id, path = model, x, cell) 
 
 
 # Assemble results for "FitBit wear time"
@@ -338,8 +349,8 @@ tab_wt <- post_wt %>%
          cell = make_cell(contrast, .lower, .upper)) %>%
   select(id, path, x, cell) 
 
-tab_med %>% spread(y, cell) %>%
-  write_csv("~/med.csv")
+tab_usab %>% spread(y, cell) %>%
+  write_csv("~/usab.csv")
 
 tab <- full_join(tab_phq8, tab_wt) %>%
   pivot_wider(names_from = "id",
